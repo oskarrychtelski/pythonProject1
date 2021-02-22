90c73a9c7207d39e185a8fefcfff6e41c3c995e9
Oskar <oskar.rychtelski@gmail.com>
2021-02-22 14:27:06 +0100

    pierwsze 3 zadania

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
-# This is a sample Python script.
+from math import *
+#zad1
+# a = 'napis'
+# b = 'napis1'
+# print(a,b)
+# c = 3
+# d = 7
+# print(c,d)
+# e = 3.14
+# f = 7.9
+# print(e,f)
+# g = 3+7j
+# h = 1+2j
+# print(g,h)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#zad2
+# def dodawanie(x,y):
+#     return x+y
+# def odejmowanie(x,y):
+#     return x-y
+# def mnozenie(x,y):
+#     return x*y
+# def dzielenie(x,y):
+#     return x/y
+#
+# print("wybierz operacje:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie")
+#
+# while True:
+#     wybor = input("podaj numer operacji(1/2/3/4): ")
+#
+#     if wybor in ('1', '2', '3', '4'):
+#         liczba1 = float(input("podaj pierwsza liczbe: "))
+#         liczba2 = float(input("podaj druga liczbe: "))
+#
+#         if wybor == '1':
+#             print(liczba1, "+", liczba2, "=", dodawanie(liczba1, liczba2))
+#
+#         elif wybor == '2':
+#             print(liczba1, "-", liczba2, "=", odejmowanie(liczba1, liczba2))
+#
+#         elif wybor == '3':
+#             print(liczba1, "*", liczba2, "=", mnozenie(liczba1, liczba2))
+#
+#         elif wybor == '4':
+#             print(liczba1, "/", liczba2, "=", dzielenie(liczba1, liczba2))
+#         break
+#     else:
+#         print("zla liczba")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# #zad3
+# a,b = 2,3
+# a+=b
+# print(a)
+# a-=b
+# print(a)
+# a*=b
+# print(a)
+# a/=b
+# print(a)
+# a**=b
+# print(a)
+# a%=b
+# print
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#zad4
+print(pow(e,10))
